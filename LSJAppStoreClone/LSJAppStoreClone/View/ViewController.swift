af784cc52d0b48613b82f03c721936386ffcdfa3
Yimkeul <leesjun29@gmail.com>
2025-05-08 11:02:59 +0900

    setting: 프로젝트 기본 셋팅(+라이브러리 추가)

---
+++ LSJAppStoreClone/LSJAppStoreClone/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LSJAppStoreClone
+//
+//  Created by yimkeul on 5/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
