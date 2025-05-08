8308a34fcb43d2800df4a3d0e0b7a2dc6f37c258
Yimkeul <leesjun29@gmail.com>
2025-05-08 16:49:37 +0900

    feat: #1 - Music Model 추가

---
+++ LSJAppStoreClone/LSJAppStoreClone/Model/Music.swift
@@ -0,0 +1,76 @@
+//
+//  Music.swift
+//  LSJAppStoreClone
+//
+//  Created by yimkeul on 5/8/25.
+//
+
+import Foundation
+
+struct Music: Codable {
+    let resultCount: Int
+    let results: [Result]
+
+    init() {
+        self.resultCount = 0
+        self.results = []
+    }
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let wrapperType: WrapperType
+    let kind: Kind
+    let artistID, collectionID, trackID: Int
+    let artistName, collectionName, trackName, collectionCensoredName: String
+    let trackCensoredName: String
+    let artistViewURL, collectionViewURL, trackViewURL: String
+    let previewURL: String
+    let artworkUrl30, artworkUrl60, artworkUrl100: String
+    let releaseDate: Date
+    let collectionExplicitness, trackExplicitness: Explicitness
+    let discCount, discNumber, trackCount, trackNumber: Int
+    let trackTimeMillis: Int
+    let country: Country
+    let currency: Currency
+    let primaryGenreName: String
+    let isStreamable: Bool
+    let collectionArtistID: Int?
+    let collectionArtistName, contentAdvisoryRating: String?
+
+    enum CodingKeys: String, CodingKey {
+        case wrapperType, kind
+        case artistID = "artistId"
+        case collectionID = "collectionId"
+        case trackID = "trackId"
+        case artistName, collectionName, trackName, collectionCensoredName, trackCensoredName
+        case artistViewURL = "artistViewUrl"
+        case collectionViewURL = "collectionViewUrl"
+        case trackViewURL = "trackViewUrl"
+        case previewURL = "previewUrl"
+        case artworkUrl30, artworkUrl60, artworkUrl100, releaseDate, collectionExplicitness, trackExplicitness, discCount, discNumber, trackCount, trackNumber, trackTimeMillis, country, currency, primaryGenreName, isStreamable
+        case collectionArtistID = "collectionArtistId"
+        case collectionArtistName, contentAdvisoryRating
+    }
+}
+
+enum Explicitness: String, Codable {
+    case explicit = "explicit"
+    case notExplicit = "notExplicit"
+}
+
+enum Country: String, Codable {
+    case kor = "KOR"
+}
+
+enum Currency: String, Codable {
+    case krw = "KRW"
+}
+
+enum Kind: String, Codable {
+    case song = "song"
+}
+
+enum WrapperType: String, Codable {
+    case track = "track"
+}
