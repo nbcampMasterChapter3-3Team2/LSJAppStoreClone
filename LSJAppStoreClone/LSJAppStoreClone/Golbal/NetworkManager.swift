3937d0cbfdd2979c3e776f5c0410579215bbe89a
Yimkeul <leesjun29@gmail.com>
2025-05-08 16:48:47 +0900

    feat: #1 - NetworkManager 구현

---
+++ LSJAppStoreClone/LSJAppStoreClone/Golbal/NetworkManager.swift
@@ -0,0 +1,47 @@
+//
+//  NetworkManager.swift
+//  LSJAppStoreClone
+//
+//  Created by yimkeul on 5/8/25.
+//
+
+import Foundation
+
+import Alamofire
+import RxSwift
+
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    private init() {}
+
+    func fetch<T: Decodable>(url: URL) -> Single<T> {
+        return Single.create { observer in
+
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+
+
+            let request = AF.request(url, method: .get)
+                .validate()
+                .responseDecodable(of: T.self, queue: .global(), decoder: decoder) { response in
+                    switch response.result {
+                    case .success(let value):
+                        observer(.success(value))
+                        NSLog("성공")
+                    case .failure(let afError):
+                        if let error = afError.asAFError, error.isResponseSerializationError {
+                            observer(.failure(NetworkErrorType.decodingFail))
+                            NSLog("디코딩 실패: \(afError)")
+                        } else {
+                            observer(.failure(NetworkErrorType.dataFetchFail))
+                            NSLog("패치 실패: \(afError)")
+                        }
+                    }
+                }
+            return Disposables.create {
+                request.cancel()
+            }
+        }
+    }
+}
