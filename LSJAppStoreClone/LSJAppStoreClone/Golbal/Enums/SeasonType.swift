cc340ba21c0d94ffbae359fbf6a6c4649f8dd06b
Yimkeul <leesjun29@gmail.com>
2025-05-09 18:40:03 +0900

    feat: #1 - MusicView CollectionLayout Header 설정

--- LSJAppStoreClone/LSJAppStoreClone/Golbal/Enums/SeasonType.swift
+++ LSJAppStoreClone/LSJAppStoreClone/Golbal/Enums/SeasonType.swift
@@ -21,4 +21,22 @@ enum Season: Int, CaseIterable {
         case .winter: return "겨울"
         }
     }
+
+    var title: String {
+        switch self {
+        case .spring: return "봄 Best"
+        case .summer: return "여름"
+        case .fall: return "가을"
+        case .winter: return "겨울"
+        }
+    }
+
+    var description: String {
+        switch self {
+        case .spring: return "봄에 어울리는 최신 음악 Best 5"
+        case .summer: return "여름에 어울리는 최신 음악"
+        case .fall: return "가을에 어울리는 최신 음악"
+        case .winter: return "겨울에 어울리는 최신 음악"
+        }
+    }
 }
--- LSJAppStoreClone/LSJAppStoreClone/View/MusicView/MusicViewSectionHeaderView.swift
+++ LSJAppStoreClone/LSJAppStoreClone/View/MusicView/MusicViewSectionHeaderView.swift
@@ -14,12 +14,12 @@ final class MusicViewSectionHeaderView: UICollectionReusableView {
     static let id = "MusicViewSectionHeader"
 
     private let titleLabel = UILabel().then {
-        $0.font = UIFont.boldSystemFont(ofSize: 20)
+        $0.font = UIFont.systemFont(ofSize: 20, weight: .bold)
         $0.textColor = .label
     }
 
     private let descLabel = UILabel().then {
-        $0.font = UIFont.boldSystemFont(ofSize: 12)
+        $0.font = UIFont.systemFont(ofSize: 12, weight: .bold)
         $0.textColor = .secondaryLabel
     }
 
@@ -45,12 +45,13 @@ final class MusicViewSectionHeaderView: UICollectionReusableView {
 
         descLabel.snp.makeConstraints {
             $0.leading.equalTo(titleLabel.snp.leading)
-            $0.top.equalTo(titleLabel.snp.bottom).offset(16)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(4)
         }
     }
 
     func configure(season: Season) {
-        titleLabel.text = season.query
+        titleLabel.text = season.title
+        descLabel.text = season.description
     }
 
 }
