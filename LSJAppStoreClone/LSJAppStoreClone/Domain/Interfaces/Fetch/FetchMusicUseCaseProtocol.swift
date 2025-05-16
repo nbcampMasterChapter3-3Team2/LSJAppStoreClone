c78f28edeac6ed4e64c54970825f55ac59602acb
Yimkeul <leesjun29@gmail.com>
2025-05-16 17:54:18 +0900

    chore: #8

--- LSJAppStoreClone/LSJAppStoreClone/Domain/Interfaces/Fetch/FetchMusicUseCaseProtocol.swift
+++ LSJAppStoreClone/LSJAppStoreClone/Domain/Interfaces/Fetch/FetchMusicUseCaseProtocol.swift
@@ -9,6 +9,5 @@ import Foundation
 import RxSwift
 
 protocol FetchMusicUseCaseProtocol {
-    /// 비즈니스 로직(정렬·필터링)을 포함해 음악 데이터를 반환합니다
     func execute(season: Season) -> Single<Music>
 }
--- LSJAppStoreClone/LSJAppStoreClone/Domain/Interfaces/Repository/MusicRepositoryProtocol.swift
+++ LSJAppStoreClone/LSJAppStoreClone/Domain/Interfaces/Repository/MusicRepositoryProtocol.swift
@@ -9,6 +9,5 @@ import Foundation
 import RxSwift
 
 protocol MusicRepositoryProtocol {
-    /// 시즌별 음악 데이터를 가져옵니다
     func fetchMusic(season: Season) -> Single<Music>
 }
